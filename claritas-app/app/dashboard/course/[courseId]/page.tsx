389bbaa828bfeb566157a6961d06f6033edcf33c
mvdhnm <mvdhnm@umsystem.edu>
2026-02-05 20:59:31 -0600

    remove unused imports in CoursePage component

--- claritas-app/app/dashboard/course/[courseId]/page.tsx
+++ claritas-app/app/dashboard/course/[courseId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { redirect, useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { CourseView } from '@/components/CourseView';
 
 interface Quiz {
